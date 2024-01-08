97562abb99f97c0c1daf6793308d8ff81e141044
pikapikaqiua <1036449976@qq.com>
2024-01-09 01:21:06 +0800

    ENH: add java base knowlege

---
+++ java_primary/hello-world-app/src/com/djicorp/speedkey/SpeedKeyDemo.java
@@ -0,0 +1,20 @@
+package com.djicorp.speedkey;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class SpeedKeyDemo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        /*
+
+         */
+        /**
+         *
+         *
+         */
+    }
+}
